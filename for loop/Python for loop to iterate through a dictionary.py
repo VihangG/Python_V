cab145878328b85be9d218f7539d87ff09b3076d
Vihang Gaokar <92376747+VihangG@users.noreply.github.com>
2024-02-15 16:02:00 +0530

    Iteration through Dictionary

---
+++ for loop/Python for loop to iterate through a dictionary.py
@@ -0,0 +1,8 @@
+programming_languages = {'J': 'JAVA', 'P': 'Python'}
+
+for i, j in programming_languages.items():
+    print(i,j)
+
+
+for a in programming_languages.keys():
+    print(a, programming_languages[a])
